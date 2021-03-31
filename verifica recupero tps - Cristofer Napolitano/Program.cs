d4b05a53c26c09e06c14424f2fdb45b7f53f2a0f
cristoferNapolitano <cristofer.napolitano@gmail.com>
2021-03-31 10:22:37 +0200

    first commit, creazione primo thread

---
+++ verifica recupero tps - Cristofer Napolitano/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace verifica_recupero_tps___Cristofer_Napolitano
+{
+    class Program
+    {
+        Thread t1, t2, t3;
+        static void Main(string[] args)
+        {
+
+        }
+
+        void Funzione1()
+        {
+            int add1=10, add2=30, somma;
+            somma = add1 + add2;
+            for(int i=0; i < 10; i++)
+            {
+                Console.WriteLine(somma);
+            }
+        }
+
+        
+    }
+}
