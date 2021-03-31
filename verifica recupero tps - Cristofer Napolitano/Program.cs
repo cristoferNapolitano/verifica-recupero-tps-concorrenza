1d8bc4fe128faeb59bd2395e99e4c8161554d0fc
cristoferNapolitano <cristofer.napolitano@gmail.com>
2021-03-31 10:45:13 +0200

    creazione terzo thread e implementazione delle funzioni nel main, debugging dei metodi funzione

--- verifica recupero tps - Cristofer Napolitano/Program.cs
+++ verifica recupero tps - Cristofer Napolitano/Program.cs
@@ -9,14 +9,24 @@ namespace verifica_recupero_tps___Cristofer_Napolitano
 {
     class Program
     {
-        Thread t1, t2, t3;
+        
         static void Main(string[] args)
         {
+            Thread t1 = new Thread(new ThreadStart(Funzione1));
+            Thread t2 = new Thread(new ThreadStart(Funzione2));
+            Thread t3 = new Thread(new ThreadStart(Funzione3));
 
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            
+            Console.ReadKey();
         }
 
-        void Funzione1()
+        static void Funzione1()
         {
+            Thread.Sleep(777);
             int add1=10, add2=30, somma;
             somma = add1 + add2;
             for(int i=0; i < 10; i++)
@@ -25,14 +35,21 @@ namespace verifica_recupero_tps___Cristofer_Napolitano
             }
         }
 
-        void Funzione2()
+        static void Funzione2()
         {
-            for (int i = 1; i < 10; i++)
+            for (int i = 1; i < 11; i++)
             {
                 Console.WriteLine(70 * i);
             }
         }
 
-        
+        static void Funzione3()
+        {
+            Thread.Sleep(777);
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Siamo nell'anno 202{i}");
+            }
+        }
     }
 }
